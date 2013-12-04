1f30f3293fe005f10cd50ca67b5e20eb0befe0a3
Sergio Afonso <sergioafonsoga@gmail.com>
2013-12-04 18:15:59 +0000

    Comprobando la respuesta de la ruta

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
 
 $(documento).ready(function() {
+	 $(".cell").click(function(event) {
+        var pathname = window.location.pathname;
+        var ruta = pathname + event.target.id;
+         $.get(ruta, function(data) {
+            if (data == 'illegal'){
+                alert("Illegal move!");
+                } 
+            else if (data.length < 3)  {
+                if (data.length > 0){
+                    $("#"+event.target.id).addClass("circle");
+                    $("#"+data).addClass( "cross" );
+                }
+                
 });
